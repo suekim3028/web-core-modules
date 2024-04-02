0e57ea870031a94a6087465905c541ef65b87838
Suebin Kim <k.sue3028@gmail.com>
2024-04-02 11:58:19 +0900

    feat: make wait util

--- src/utils/common.utils.ts
+++ src/utils/common.utils.ts
@@ -1,1 +1,9 @@
 //
+
+export const wait = (seconds: number) => {
+  return new Promise((resolve: (v: null) => void) => {
+    setTimeout(() => {
+      resolve(null);
+    }, seconds * 1000);
+  });
+};
