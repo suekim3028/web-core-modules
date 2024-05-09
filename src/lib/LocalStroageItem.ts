01741c67e97457b00ede335735e2fa597a3751fa
Suebin Kim <k.sue3028@gmail.com>
2024-05-09 10:36:46 +0900

    refactor: refactor storage item type

--- src/lib/LocalStroageItem.ts
+++ src/lib/LocalStroageItem.ts
@@ -8,10 +8,14 @@ export class LocalStorageItem<T extends object> {
   public get() {
     const item = localStorage.getItem(this.key);
     if (!item) return null;
-    return JSON.parse(item) as T;
+    const parsedItem = JSON.parse(item);
+    if (typeof parsedItem != "object") return null;
+    return parsedItem as Partial<T>;
   }
 
-  public set(data: T) {
-    localStorage.setItem(this.key, JSON.stringify(data));
+  public set(data: Partial<T>) {
+    const item = this.get();
+    const updated = item ? { ...item, ...data } : data;
+    localStorage.setItem(this.key, JSON.stringify(updated));
   }
 }
