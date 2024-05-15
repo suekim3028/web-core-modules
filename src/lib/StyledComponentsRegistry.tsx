809f9600c970fc3b27d8878e3414707f953bf1be
Suebin Kim <k.sue3028@gmail.com>
2024-05-15 14:32:09 +0900

    feat: memo styled

--- src/lib/StyledComponentsRegistry.tsx
+++ src/lib/StyledComponentsRegistry.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import React, { useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
+import React, { useRef } from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
-export default function StyledComponentsRegistry({
+const StyledComponentsRegistry = ({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}) => {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+  const styledComponentsStyleSheet = useRef(new ServerStyleSheet()).current;
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
@@ -26,4 +26,6 @@ export default function StyledComponentsRegistry({
       {children}
     </StyleSheetManager>
   );
-}
+};
+
+export default React.memo(StyledComponentsRegistry);
