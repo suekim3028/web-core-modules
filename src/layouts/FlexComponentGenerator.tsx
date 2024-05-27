453a6bcc59f5d05e3d6d23d04f6f96efdab1f2dc
Suebin Kim <k.sue3028@gmail.com>
2024-05-28 08:25:49 +0900

    feat: add ref forwarding to flex

--- src/layouts/FlexComponentGenerator.tsx
+++ src/layouts/FlexComponentGenerator.tsx
@@ -1,22 +1,20 @@
 import { Flex as FlexComponent, FlexProps } from "@chakra-ui/react";
+import { forwardRef } from "react";
 
-const FlexComponentGenerator =
-  <
-    U extends Record<string, string>,
-    S extends U extends Record<infer V, string> ? V : string
-  >(
-    colorGenerator: U
-  ) =>
-  ({
-    bgColor,
-    bgRgbColor,
-    ...props
-  }: FlexProps & { bgColor?: S; bgRgbColor?: string }) =>
-    (
+const FlexComponentGenerator = <
+  U extends Record<string, string>,
+  S extends U extends Record<infer V, string> ? V : string
+>(
+  colorGenerator: U
+) =>
+  forwardRef<HTMLDivElement, FlexProps & { bgColor?: S; bgRgbColor?: string }>(
+    ({ bgColor, bgRgbColor, ...props }, ref) => (
       <FlexComponent
+        ref={ref}
         {...props}
         bgColor={bgColor ? colorGenerator[bgColor] : bgRgbColor}
       />
-    );
+    )
+  );
 
 export default FlexComponentGenerator;
